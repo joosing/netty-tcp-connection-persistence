5c90de7c7a9df15c1630c9a5cc56a6cdc76b59ee
jsing <joosing711@gmail.com>
2022-05-01 17:30:11 +0900

    Init the project

---
+++ src/test/java/netty/tcp/connection/persistence/ApplicationTests.java
@@ -0,0 +1,13 @@
+package netty.tcp.connection.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
