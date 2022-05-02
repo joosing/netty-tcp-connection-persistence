b72812b84dc5d511d0edf6cd771c429774021e1a
jsing <joosing711@gmail.com>
2022-05-02 23:08:03 +0900

    Implement a ForwardHandler

---
+++ src/main/java/netty/tcp/connection/persistence/ForwardHandler.java
@@ -0,0 +1,17 @@
+package netty.tcp.connection.persistence;
+
+import java.util.concurrent.BlockingQueue;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ForwardHandler extends SimpleChannelInboundHandler<String> {
+    private final BlockingQueue<String> forwardQueue;
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        forwardQueue.put(msg);
+        ctx.fireChannelRead(msg);
+    }
+}
