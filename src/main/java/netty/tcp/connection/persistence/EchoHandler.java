8f451a40186555d46971b356d3b9bd0dd6f6a0fc
jsing <joosing711@gmail.com>
2022-05-02 23:07:54 +0900

    Implement a EchoHandler

---
+++ src/main/java/netty/tcp/connection/persistence/EchoHandler.java
@@ -0,0 +1,12 @@
+package netty.tcp.connection.persistence;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class EchoHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        ctx.channel().writeAndFlush(msg);
+        ctx.fireChannelRead(msg);
+    }
+}
